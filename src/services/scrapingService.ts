2f6ee058bf838ab9620b7f96215e2d3db68ffec4
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2025-08-27 13:38:37 -0400

    fix: add args to chromium browser and close conexion in error catch

--- src/services/scrapingService.ts
+++ src/services/scrapingService.ts
@@ -5,44 +5,53 @@ export class ScrapingService {
   static async getDaysForDrome(): Promise<string> {
     const dromeTime = await this.getDromeTime();
 
-    // Aquí puedes procesar dromeTime para obtener los días
     if (dromeTime === "No encontrado"){
-      return dromeTime
+      return dromeTime;
     }
 
     const daysSegmento = dromeTime.split(' ')[0]; 
-
     return daysSegmento;
   }
 
   static async getDromeTime(): Promise<string> {
-
     const url = 'https://tibia.fandom.com/wiki/Tibiadrome/Rotation';
     const xpath = '/html/body/div[4]/div[4]/div[2]/main/div[3]/div/div/center/table/tbody/tr[3]/td/b';
+    let browser;
 
     try {
-      const browser = await puppeteer.launch({
+      browser = await puppeteer.launch({
         headless: true,
-        executablePath: '/usr/bin/chromium-browser',  
-        args: ['--no-sandbox']
+        executablePath: '/usr/bin/chromium-browser',
+        // Argumentos para optimizar en entornos con pocos recursos y evitar errores de sandbox
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage', // Crítico para entornos con poca memoria
+          '--disable-accelerated-2d-canvas',
+          '--no-first-run',
+          '--no-zygote',
+          '--single-process', // Puede ayudar en entornos con poca memoria
+          '--disable-gpu'
+        ]
       });
       const page = await browser.newPage();
-      await page.goto(url, { waitUntil: 'networkidle2' });
+      // Aumentamos el timeout a 60 segundos
+      await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
 
-      // Se usa `waitForSelector` con el prefijo 'xpath/' para esperar el elemento.
       const element = await page.waitForSelector(`xpath/${xpath}`);
-      let text: string;
+      let text = 'No encontrado';
       if (element) {
-        // Se obtiene el texto del elemento.
         text = await page.evaluate(el => el?.textContent ?? 'No encontrado', element);
-      } else {
-        text = 'No encontrado';
       }
 
       await browser.close();
       return text;
     } catch (error) {
       console.error('Error al obtener el tiempo del Drome:', error);
+      // Asegurarse de que el navegador se cierre si hay un error
+      if (browser) {
+        await browser.close();
+      }
       return 'No encontrado';
     }
   }
